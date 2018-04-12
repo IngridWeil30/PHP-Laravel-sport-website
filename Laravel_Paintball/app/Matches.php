91cdfb1258905d22f6efe0645d0e3c2e1aef7ac6
ingridweil <ingrid.weil@coding-academy.fr>
2018-04-12 16:07:39 +0200

    added display_match(id) with view

---
+++ Laravel_Paintball/app/Matches.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Matches extends Model
+{
+    protected $table = 'matches';
+    //protected $fillable = ['team1']
+    //public $timestamps = false;
+/*    public static function find($id){
+        dd($id);
+    }*/
+}
--- Laravel_Paintball/resources/views/matches.blade.php
+++ Laravel_Paintball/resources/views/matches.blade.php
@@ -1,5 +1,11 @@
 @extends('default')
 
 @section('content')
-<p>Test</p>
+    <p>Matches Table</p>
+
+<p>Status : {{$matches->matchStatus}}.</p>
+<p>Team ID {{$matches->id}} T1 {{$matches->team1}} VS T2 {{$matches->team2}} in {{$matches->city}} city.</p>
+<p>Match description {{$matches->description}}.</p>
+<p>Placement {{$matches->placement}}.</p>
+<p>Winner is {{$matches->winner}}, final score T1 is {{$matches->scoreTeam1}} and T2 is {{$matches->scoreTeam2}}.</p>
 @endsection
