94febbfde6c70a3c82980f4172e973201eb83e10
ingridweil <ingrid.weil@coding-academy.fr>
2018-04-12 17:40:36 +0200

    added ingrid files

--- Laravel_Paintball/resources/views/matches.blade.php
+++ Laravel_Paintball/resources/views/matches.blade.php
@@ -1,11 +1,26 @@
 @extends('default')
-
 @section('content')
-    <p>Matches Table</p>
-
-<p>Status : {{$matches->matchStatus}}.</p>
-<p>Team ID {{$matches->id}} T1 {{$matches->team1}} VS T2 {{$matches->team2}} in {{$matches->city}} city.</p>
-<p>Match description {{$matches->description}}.</p>
-<p>Placement {{$matches->placement}}.</p>
-<p>Winner is {{$matches->winner}}, final score T1 is {{$matches->scoreTeam1}} and T2 is {{$matches->scoreTeam2}}.</p>
+    <h2>Matches Table</h2>
+        <table class="table table-responsive table-dark">
+            <tr>
+                <td>Match ID</td>
+                <td>Team 1</td>
+                <td>Team 2</td>
+                <td>Winner</td>
+                <td>Final score Team 1</td>
+                <td>Final score Team 2</td>
+                <td>City</td>
+                <td>Description</td>
+                <td>Status</td>
+            </tr>
+                <td>{{$matches->id}}</td>
+                <td>{{$matches->team1}}</td>
+                <td>{{$matches->team2}}</td>
+                <td>{{$matches->winner}}</td>
+                <td>{{$matches->scoreTeam1}}</td>
+                <td>{{$matches->scoreTeam2}}</td>
+                <td>{{$matches->city}}</td>
+                <td>{{$matches->description}}</td>
+                <td>{{$matches->matchStatus}}</td>
+        </table>
 @endsection
